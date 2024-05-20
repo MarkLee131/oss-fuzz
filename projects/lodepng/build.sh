6397f407dd2c7e6a6ba302ea9aef2d039861124c
MarkLee131 <1512543916@qq.com>
2024-05-20 15:51:01 +0800

    Update build.sh

--- projects/lodepng/build.sh
+++ projects/lodepng/build.sh
@@ -20,6 +20,9 @@
 $CXX $CXXFLAGS -DLODEPNG_MAX_ALLOC=100000000 lodepng.cpp lodepng_fuzzer.cpp -o\
  $OUT/lodepng_fuzzer $LIB_FUZZING_ENGINE
 
+$CXX $CXXFLAGS -DLODEPNG_MAX_ALLOC=100000000 lodepng.cpp fuzz_encode32_file.cpp -o\
+ $OUT/fuzz_encode32_file $LIB_FUZZING_ENGINE
+
 # copy seed corpus
 cp $SRC/lodepng_fuzzer_seed_corpus.zip $OUT/
 
